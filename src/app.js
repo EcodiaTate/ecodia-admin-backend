5810b57d8c1c28dc18c150be01500be2520ac16d
EcodiaTate <tate@ecodia.au>
2026-04-01 15:38:31 +1000

    Add CORS for Vercel frontend, make env vars optional for phased setup

--- src/app.js
+++ src/app.js
@@ -17,7 +17,13 @@ const app = express()
 
 // Middleware
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://admin.ecodia.au',
+    'http://localhost:5173',
+  ],
+  credentials: true,
+}))
 app.use(compression())
 app.use(express.json({ limit: '5mb' }))
 
